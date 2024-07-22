478df1daa8dcf9584c6ffc05fdcca7c644180a4a
KevColomaDev <kev.enrri@hotmail.com>
2024-07-22 07:06:58 -0500

    refactor: remove requiered field from inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,7 +47,7 @@ export const Login = () => {
               autoComplete="email"
               placeholder="user@example.com"
               className="block w-full rounded-md border-2 py-1.5 px-2 text-gray-900 shadow-sm ring-0.5 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1.5 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              {...register("email", { required: true })}
+              {...register("email")}
             />
           </div>
 
@@ -61,7 +61,7 @@ export const Login = () => {
               autoComplete="current-password"
               placeholder="••••••••"
               className="block w-full rounded-md border-2 py-1.5 px-2 text-gray-900 shadow-sm ring-0.5 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1.5 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              {...register("password", { required: true })}
+              {...register("password")}
             />
           </div>
 
