f5ffd91d25f33dee77a8be48ab7dd1618633723b
KevColomaDev <kev.enrri@hotmail.com>
2024-07-26 07:34:38 -0500

    fix: save cookie

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 const administratorAPI = 'http://localhost:4321/administrators'
+// const administratorAPI = 'https://backend-hospital-0f3d.onrender.com/administrators'
 
 export const loginRequest = async (user) => {
-  const response = await axios.post(`${administratorAPI}/login`, user)
+  const response = await axios.post(`${administratorAPI}/login`, user, { withCredentials: true })
   return console.log(response.data)
 }
